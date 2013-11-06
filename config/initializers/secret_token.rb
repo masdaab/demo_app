2993a3c1f4ecd925defaf291d009be491155d7c0
zinux <zinux@zinux-0S.(none)>
2013-11-06 09:54:41 +0700

    init demo apps

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = 'a6c69973d835a3806d3c6fcd9e2918322cb88a4726529121939f8e52a65343e211fa4709226699c7210419fdd6a614bb12cd5b7ed7445e1a4ffc7ca0a72de0a4'
